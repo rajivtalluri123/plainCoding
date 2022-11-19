d719526238d7b2073a5dfc284da282164bc20e21
Rajiv Talluri <tallurirajiv@gmail.com>
2022-11-19 14:37:52 -0500

    Adding lld and matrix traverse

---
+++ src/main/java/LLDesigns/LogAggregationSystem.java
@@ -0,0 +1,61 @@
+package LLDesigns;
+//Implement a Log Aggregation System which aggregates logs from various services in a datacenter and provides search APIs.
+//
+//Design the LogAggregator class:
+//
+//LogAggregator(int machines, int services) Initializes the object with machines and services representing the number of machines and services in the datacenter, respectively.
+//void pushLog(int logId, int machineId, int serviceId, String message) Adds a log with id logId notifying that the machine machineId sent a string message while executing the service serviceId.
+//List<Integer> getLogsFromMachine(int machineId) Returns a list of ids of all logs added by machine machineId.
+//List<Integer> getLogsOfService(int serviceId) Returns a list of ids of all logs added while running service serviceId on any machine.
+//List<String> search(int serviceId, String searchString) Returns a list of messages of all logs added while running service serviceId where the message of the log contains searchString as a substring.
+
+//notes
+// The entries in each list should be in the order they were added, i.e., the older logs should precede the newer logs.
+//A machine can run multiple services more than once. Similarly, a service can be run on multiple machines.
+//All logId may not be ordered.
+//A substring is a contiguous sequence of characters within a string.
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class LogAggregator {
+    Map<Integer, List<Integer>> machineMessages; // mackineId and list of logIds
+    Map<Integer, List<Integer>> serviceMessages; // serviceId and list of logIds
+    Map<Integer, String>        logs;
+
+    public LogAggregator() {
+        machineMessages = new HashMap<>();
+        serviceMessages = new HashMap<>();
+        logs = new HashMap<>();
+    }
+
+    public List<Integer> getLogsFromMachine(int machineId) {
+        return machineMessages.getOrDefault(machineId, new ArrayList());
+    }
+
+    public List<Integer> getLogsOfService(int serviceId) {
+        return serviceMessages.getOrDefault(serviceId, new ArrayList());
+    }
+
+    public List<String> search(int serviceId, String searchString) {
+        List<String> res = new ArrayList<String>();
+
+        for(Integer log : serviceMessages.getOrDefault(serviceId, new ArrayList<Integer>())) {
+            if(logs.get(log).contains(searchString))
+                res.add(logs.get(log));
+        }
+        return res;
+    }
+
+}
+
+/**
+ * Your LogAggregator object will be instantiated and called as such:
+ * LogAggregator obj = new LogAggregator();
+ * obj.pushLog(logId,machineId,serviceId,message);
+ * List<Integer> param_2 = obj.getLogsFromMachine(machineId);
+ * List<Integer> param_3 = obj.getLogsOfService(serviceId);
+ * List<String> param_4 = obj.search(serviceId,searchString);
+ */
--- src/main/java/traversal/MatrixTraversalLeet.java
+++ src/main/java/traversal/MatrixTraversalLeet.java
@@ -1,7 +1,6 @@
 package traversal;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class MatrixTraversalLeet {
     int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};  //left, right, down, up
@@ -45,5 +44,57 @@ public class MatrixTraversalLeet {
             level++;
         }
         //all possible rooms are updated by now
+    }
+
+    //417. pacific and atlantic water flow
+    // There is an m x n rectangular island that borders both the Pacific Ocean and Atlantic Ocean.
+    // The Pacific Ocean touches the island's left and top edges, and the Atlantic Ocean touches the island's right and bottom edges.
+    //The island is partitioned into a grid of square cells. You are given an m x n integer matrix heights
+    // where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
+    //The island receives a lot of rain, and the rain water can flow to neighboring cells directly north, south, east, and west if the neighboring cell's height is less than or equal to the current cell's height.
+    // Water can flow from any cell adjacent to an ocean into the ocean.
+    //
+    //Return a 2D list of grid coordinates result where result[i] = [ri, ci] denotes that rain water can flow from cell (ri, ci) to both the Pacific and Atlantic oceans.
+
+    //i/p - heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+    // o/p - [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
+    //alg- do dfs from both oceans side and see how many cells are reachable to pacific and how many are reachable to atlantic-- cell with both oceans reachable is our guy
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int rows = heights.length;
+        int cols = heights[0].length;
+
+        int[][] pacific = new int[rows][cols];
+        int[][] atlantic = new int[rows][cols];
+
+        //do dfs from left side and right side
+        for(int i =0; i < rows; i++) {
+            dfs(pacific, i, 0, heights);
+            dfs(atlantic, i, cols -1, heights);
+        }
+        // do dfs from top and bottom side
+        for(int i =0; i < cols; i++) {
+            dfs(pacific, 0, i, heights);
+            dfs(atlantic, rows -1, i, heights );
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i =0; i < rows; i++) {
+            for(int j =0; j < cols; j++) {
+                if(pacific[i][j] == 1 && atlantic[i][j] == 1)
+                    res.add(Arrays.asList(i, j));
+            }
+        }
+        return res;
+    }
+    private void dfs(int[][] reachable, int row, int col, int[][] heights) {
+        reachable[row][col] = 1;
+        for(int[] dir : directions) {
+            int nextRow = row + dir[0];
+            int nextCol = col + dir[1];
+            if(nextRow >=0 && nextRow < heights.length && nextCol >=0 && nextCol < heights[0].length
+                    && heights[nextRow][nextCol] >= heights[row][col]) {
+                //as we started dfs from oceans the check is reverse >= next height should be gt to  have water flowing downstream into ocean
+                dfs(reachable, nextRow, nextCol, heights);
+            }
+        }
     }
 }
